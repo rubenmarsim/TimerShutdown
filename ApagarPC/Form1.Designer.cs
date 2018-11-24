861bfcc7e91257c7d17b7d3515860056254129f9
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-24 17:28:18 +0100

    .

---
+++ ApagarPC/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ApagarPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnShutdown = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtBoxMin = new System.Windows.Forms.TextBox();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(724, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnShutdown
+            // 
+            this.btnShutdown.Location = new System.Drawing.Point(189, 352);
+            this.btnShutdown.Name = "btnShutdown";
+            this.btnShutdown.Size = new System.Drawing.Size(75, 23);
+            this.btnShutdown.TabIndex = 1;
+            this.btnShutdown.Text = "Apagar PC";
+            this.btnShutdown.UseVisualStyleBackColor = true;
+            this.btnShutdown.Click += new System.EventHandler(this.btnShutdown_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(39, 352);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtBoxMin
+            // 
+            this.txtBoxMin.Location = new System.Drawing.Point(189, 273);
+            this.txtBoxMin.Name = "txtBoxMin";
+            this.txtBoxMin.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxMin.TabIndex = 3;
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(12, 273);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(170, 13);
+            this.lblMin.TabIndex = 4;
+            this.lblMin.Text = "Minutos para que se apague el PC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.txtBoxMin);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnShutdown);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnShutdown;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtBoxMin;
+        private System.Windows.Forms.Label lblMin;
+    }
+}
+
---
+++ ApagarPC/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApagarPC
+{
+    public partial class Form1 : Form
+    {
+        const string strFileName="shutdown";
+        const string strQuery = "/s /t ";
+        string strTime = "";
+        int valMin = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(strFileName, "/a");
+        }
+
+        private void btnShutdown_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(strFileName, strQuery+strTime);
+        }
+    }
+}
