336f9e729a950824efc40d51a6f8b4a5f6df3d11
Rubén Martínez <ruben.marsim10@gmail.com>
2018-11-25 11:09:24 +0100

    .

--- ApagarPC/Form1.Designer.cs
+++ ApagarPC/Form1.Designer.cs
@@ -41,9 +41,12 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(725, 0);
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.AutoSize = true;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.btnExit.Location = new System.Drawing.Point(1762, 0);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(82, 41);
             this.btnExit.TabIndex = 0;
             this.btnExit.Text = "Salir";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -51,9 +54,11 @@
             // 
             // btnShutdown
             // 
-            this.btnShutdown.Location = new System.Drawing.Point(39, 365);
+            this.btnShutdown.AutoSize = true;
+            this.btnShutdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.btnShutdown.Location = new System.Drawing.Point(414, 765);
             this.btnShutdown.Name = "btnShutdown";
-            this.btnShutdown.Size = new System.Drawing.Size(75, 23);
+            this.btnShutdown.Size = new System.Drawing.Size(181, 41);
             this.btnShutdown.TabIndex = 1;
             this.btnShutdown.Text = "Apagar PC";
             this.btnShutdown.UseVisualStyleBackColor = true;
@@ -61,9 +66,11 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(137, 365);
+            this.btnCancel.AutoSize = true;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.btnCancel.Location = new System.Drawing.Point(189, 765);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(138, 44);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Cancelar";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -71,58 +78,78 @@
             // 
             // txtBoxMin
             // 
-            this.txtBoxMin.Location = new System.Drawing.Point(112, 270);
+            this.txtBoxMin.BackColor = System.Drawing.SystemColors.GrayText;
+            this.txtBoxMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.txtBoxMin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBoxMin.Location = new System.Drawing.Point(414, 619);
             this.txtBoxMin.Name = "txtBoxMin";
-            this.txtBoxMin.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxMin.Size = new System.Drawing.Size(181, 38);
             this.txtBoxMin.TabIndex = 3;
             // 
             // lblMin
             // 
             this.lblMin.AutoSize = true;
-            this.lblMin.Location = new System.Drawing.Point(50, 270);
+            this.lblMin.BackColor = System.Drawing.SystemColors.GrayText;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.lblMin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblMin.Location = new System.Drawing.Point(210, 626);
             this.lblMin.Name = "lblMin";
-            this.lblMin.Size = new System.Drawing.Size(44, 13);
+            this.lblMin.Size = new System.Drawing.Size(117, 31);
             this.lblMin.TabIndex = 4;
-            this.lblMin.Text = "Minutos";
+            this.lblMin.Text = "Minutos:";
             // 
             // lblSegundos
             // 
             this.lblSegundos.AutoSize = true;
-            this.lblSegundos.Location = new System.Drawing.Point(39, 314);
+            this.lblSegundos.BackColor = System.Drawing.SystemColors.GrayText;
+            this.lblSegundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.lblSegundos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSegundos.Location = new System.Drawing.Point(183, 687);
             this.lblSegundos.Name = "lblSegundos";
-            this.lblSegundos.Size = new System.Drawing.Size(55, 13);
+            this.lblSegundos.Size = new System.Drawing.Size(144, 31);
             this.lblSegundos.TabIndex = 5;
-            this.lblSegundos.Text = "Segundos";
+            this.lblSegundos.Text = "Segundos:";
             // 
             // lblHoras
             // 
             this.lblHoras.AutoSize = true;
-            this.lblHoras.Location = new System.Drawing.Point(59, 227);
+            this.lblHoras.BackColor = System.Drawing.SystemColors.GrayText;
+            this.lblHoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.lblHoras.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblHoras.Location = new System.Drawing.Point(232, 561);
             this.lblHoras.Name = "lblHoras";
-            this.lblHoras.Size = new System.Drawing.Size(35, 13);
+            this.lblHoras.Size = new System.Drawing.Size(95, 31);
             this.lblHoras.TabIndex = 6;
-            this.lblHoras.Text = "Horas";
+            this.lblHoras.Text = "Horas:";
             // 
             // txtBoxHoras
             // 
-            this.txtBoxHoras.Location = new System.Drawing.Point(112, 227);
+            this.txtBoxHoras.BackColor = System.Drawing.SystemColors.GrayText;
+            this.txtBoxHoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.txtBoxHoras.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBoxHoras.Location = new System.Drawing.Point(414, 554);
             this.txtBoxHoras.Name = "txtBoxHoras";
-            this.txtBoxHoras.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxHoras.Size = new System.Drawing.Size(181, 38);
             this.txtBoxHoras.TabIndex = 7;
             // 
             // txtBoxSecs
             // 
-            this.txtBoxSecs.Location = new System.Drawing.Point(112, 314);
+            this.txtBoxSecs.BackColor = System.Drawing.SystemColors.GrayText;
+            this.txtBoxSecs.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.txtBoxSecs.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBoxSecs.Location = new System.Drawing.Point(414, 684);
             this.txtBoxSecs.Name = "txtBoxSecs";
-            this.txtBoxSecs.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxSecs.Size = new System.Drawing.Size(181, 38);
             this.txtBoxSecs.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
             this.BackgroundImage = global::ApagarPC.Properties.Resources.DisneyWallpaperRosa;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1843, 1061);
             this.Controls.Add(this.txtBoxSecs);
             this.Controls.Add(this.txtBoxHoras);
             this.Controls.Add(this.lblHoras);
@@ -134,6 +161,7 @@
             this.Controls.Add(this.btnExit);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
             this.PerformLayout();
 
